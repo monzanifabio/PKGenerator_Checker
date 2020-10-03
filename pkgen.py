85c048f4c8b8b8c4d5ce9714f5f31492db980938
Fabio Monzani <renderbros@gmail.com>
2020-10-03 23:53:59 +0100

    Change execution

--- pkgen.py
+++ pkgen.py
@@ -62,16 +62,16 @@ while True:
 		convert = int(req.content)
 		# Comment this next line if you don't want to save all the generated addresses
 		collect_addresses()
+		#Print the address balance
+		print convert
+		#If the address balance is more than 0 it creates a file and store public address and balance
+		if convert > 0:
+		    print 'We found something!'
+		    file = open('results.txt', 'a')
+		    file.write(wif + '\n' + publicAddress + '\n' + str(convert) + '\n')
+		    file.close()
+		else:
+		    print 'Empty'
 	except ValueError:
 		print 'Timeout, lets have a break'
 		time.sleep(5)
-	#Print the address balance
-	print convert
-	#If the address balance is more than 0 it creates a file and store public address and balance
-	if convert > 0:
-	    print 'We found something!'
-	    file = open('results.txt', 'a')
-	    file.write(wif + '\n' + publicAddress + '\n' + str(convert) + '\n')
-	    file.close()
-	else:
-	    print 'Empty'
