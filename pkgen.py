9a5d102489dba5b98ca881fab34596438a8bac15
Fabio Monzani <renderbros@gmail.com>
2024-12-15 21:27:14 +0000

    Updated to py3

--- pkgen.py
+++ pkgen.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-import os
+import secrets
+import json
+import codecs
 import ecdsa
 import hashlib
 import base58
@@ -9,69 +11,102 @@ import time
 from smtplib import SMTP_SSL as SMTP
 import logging
 
-
 wif = ""
 
-def collect_addresses():
-	file = open('collection.txt', 'a')
-	file.write(wif + '\n' + publicAddress + '\n')
-	file.close()
+logging.basicConfig(filename='BTC_PrivateKeys_'+time.strftime("%Y-%m-%d-%H-%M")+'.csv', \
+level=logging.INFO, format='%(message)s', datefmt='%Y-%m-%d,%H:%M:%S')
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("blockcypher").setLevel(logging.WARNING)
+logging.info ('"Date" "Time" "Key" "PublicAddress" "Transactions"')
+
+
 
 def ping_address(publicAddress):
 	global pk
 	global wif
 	global publicKey
 
+	"""
+	sends Request to a Block Explorer	
+	
+	NEEDS a good block explorer API to work with as blockcypher and blockchain.com have API limits.
+	"""
+
+	
+	resp = requests.get("https://blockstream.info/api/address/"+ publicAddress)
+	# resp = requests.get("https://blockchain.info/q/addressbalance/"+ publicAddress)
+	
+
+	if resp.ok:
+		ourJSON = resp.json()
+		trans = dict(ourJSON['chain_stats'])['tx_count']
+		balance = dict(ourJSON['chain_stats'])['funded_txo_sum']
+		print( balance )
+
+	else:
+		print(resp.text)
+		raise ValueError("cannot reach block explorer for balance", resp)
+   
+	# "WifKey", "HexKey", "PublicAddress", "PublicKey", "Balance"
+	#Comment out this line if you wish to NOT record blank keys
+	# logging.info (time.strftime("%d-%m-%y %H:%M ") + wif.decode('utf-8') +" "+ publicAddress +" "+ str(trans))
+
+	if float(balance) > 0.00000000:
+		logging.info (''+ time.strftime("%m-%d-%y %H:%M:%S") +','+ wif.decode('utf-8') +','+publicAddress+' ,balance '+str(balance))
+		
+		print( "Congratulations...alert the world cause you just made some sort of history friend!" )
+		print(wif.decode('utf-8'))
+		with open('results.txt', 'a+') as f:
+			f.write(''+ time.strftime("%m-%d-%y %H:%M:%S") +','+ wif.decode('utf-8') +','+publicAddress+' ,balance '+str(balance))
+			f.close
+
+
 def wif_conversion(pk):
 	global wif
 	padding = '80' + pk
-	# print padding
+	# print( padding )
 
-	hashedVal = hashlib.sha256(padding.decode('hex')).hexdigest()
-	checksum = hashlib.sha256(hashedVal.decode('hex')).hexdigest()[:8]
-	# print hashedVal
-	# print padding+checksum
+	hashedVal = hashlib.sha256(codecs.decode(padding, 'hex')).hexdigest()
+	checksum = hashlib.sha256(codecs.decode(hashedVal, 'hex')).hexdigest()[:8]
+	# print( hashedVal )
+	# print( padding+checksum )
 
 	payload = padding + checksum
-	wif = base58.b58encode(payload.decode('hex'))
-	print wif
-
+	wif = base58.b58encode(codecs.decode(payload, 'hex'))
+	print( wif.decode('utf-8') )
+	
 
 while True:
 
-	pk = os.urandom(32).encode("hex")
+	pk = secrets.token_hex(32)
 	wif_conversion(pk)
 
-	sk = ecdsa.SigningKey.from_string(pk.decode("hex"), curve = ecdsa.SECP256k1)
-	vk = sk.verifying_key
-	publicKey = ("\04" + vk.to_string())
+	sk = ecdsa.SigningKey.from_string(codecs.decode(pk, "hex"), curve=ecdsa.SECP256k1)
+	vk = sk.get_verifying_key()
+	publicKey = "\04" + str(vk.to_string())
 	ripemd160 = hashlib.new('ripemd160')
-	ripemd160.update(hashlib.sha256(publicKey).digest())
-	networkAppend = '\00' + ripemd160.digest()
+	ripemd160.update(hashlib.sha256(codecs.encode(publicKey)).digest())
+	networkAppend = b'\00' + ripemd160.digest()
 	checksum = hashlib.sha256(hashlib.sha256(networkAppend).digest()).digest()[:4]
 	binary_address = networkAppend + checksum
 	publicAddress = base58.b58encode(binary_address)
-	print publicAddress
-	#Store the ulr and the public address
-	url = 'https://blockchain.info/q/addressbalance/'+publicAddress
-	#Check the public address on blockchain.com to see the balance
-	req = requests.get(url)
-	#In case blockchain.com throws an error for to many requests
-	try:
-		#Convert the balance returned as an integer
-		convert = int(req.content)
-		# Comment this next line if you don't want to save all the generated addresses
-		collect_addresses()
-	except ValueError:
-		print 'Timeout, lets have a break'
-		time.sleep(5)
-	#Print the address balance
-	print convert
-	#If the address balance is more than 0 it creates a file and store public address and balance
-	if convert > 0:
-	    print 'We found something!'
-	    file = open('results.txt', 'a')
-	    file.write(wif + '\n' + publicAddress + '\n' + str(convert) + '\n')
-	    file.close()
-	else:
-	    print 'Empty'
+	print( publicAddress.decode('utf-8') )
+	while True:
+		try:
+			ping_address(publicAddress.decode('utf-8'))
+			# probably does nothing...who knows ;)
+			time.sleep(.47)	
+		except ValueError:
+			print( "Aaaannnnd we got Timed Out" )
+			print( pk )
+			print( publicAddress )
+			time.sleep(3)
+			continue
+		except KeyError:
+			print( "we may be denied or something, keep the script moving" )
+			time.sleep(10)			
+		break
+
+# msg = "I own your Private Key for %s" %(publicAddress)
+# signed_msg = sk.sign(msg)
+# encoded_msg = signed_msg.encode("hex")
